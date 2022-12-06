7b431795b3a85b21b26c4026678f1a433fd69c00
arashi4ever <fxiao.mecheng@gmail.com>
2022-12-06 15:33:37 -0500

    Edit test script to work with ES6 import

--- tests/note.test.js
+++ tests/note.test.js
@@ -1,1 +1,10 @@
+import {getNotes, loadNotes} from '.././notes.js'
+
+test('hello node note', () => {
+  expect(getNotes).toBeDefined()
+})
  
+test('this should fail',() => {
+  throw new Error('failure')
+})   
+
